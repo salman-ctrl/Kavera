ba573e4a794bacfc15326a88ba0bb9eec456ceb6
salman-ctrl <zirmanvictory@gmail.com>
2025-03-27 21:40:19 +0700

    edit card

--- src/assets/components/HalamanProduct.jsx
+++ src/assets/components/HalamanProduct.jsx
@@ -26,6 +26,13 @@ const HalamanProduct = () => {
                         <p>star</p>
                     </div>
                 </div>
+                <div className='w-50 h-50'>
+                    <img src="#" alt="" />
+                    <div>
+                        <p>Sepatu</p>
+                        <p>star</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
