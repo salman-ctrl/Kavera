4864877844c630006acabcf0c1d635c493c60724
salman-ctrl <zirmanvictory@gmail.com>
2025-03-27 21:20:07 +0700

    create nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-
+      <h1 className='text-red-400'>halo world</h1>
     </div>
   )
 }
---
+++ src/assets/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className='flex '>
+                <a href="#">Home</a>
+                <a href="#">Keranjang</a>
+                <a href="#">Favorit</a>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
