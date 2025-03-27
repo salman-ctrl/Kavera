2cc280b59878f20941f73dbf280dbba92151f452
salman-ctrl <zirmanvictory@gmail.com>
2025-03-27 21:38:51 +0700

    add card

--- src/assets/components/HalamanProduct.jsx
+++ src/assets/components/HalamanProduct.jsx
@@ -6,7 +6,11 @@ const HalamanProduct = () => {
             <h1>Our Product</h1>
             <div>
                 <div className='w-50 h-50'>
-
+                    <img src="#" alt="" />
+                    <div>
+                        <p>Sepatu</p>
+                        <p>star</p>
+                    </div>
                 </div>
             </div>
         </div>
